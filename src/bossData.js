87dbc9bc91b95f77674b5b377e1430ce75efe84b
ajSeadler <aj6287@gmail.com>
2024-08-01 22:00:58 -0500

    update boss info

--- src/bossData.js
+++ src/bossData.js
@@ -7,7 +7,7 @@ const bossData = [
     description:
       "Eustace, the self-proclaimed 'King of Crust,' emerged from the depths of obscurity with a battle-ready arsenal caked in muck and rebellion. With a personality as polished as his dirty gear, he's here to make a mess of things and collect your Runes while he’s at it. Approach with caution—his presence is as subtle as a sledgehammer in a library.",
     location: "Toilet",
-    drops: ["10,000 Runes", "Inquisitor's Girandole"],
+    drops: ["420,666 Runes", "Inquisitor's Grand Hole"],
     healthPoints: "???",
   },
 
